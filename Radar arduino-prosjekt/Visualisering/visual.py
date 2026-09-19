61546834f38b0934606eeb88a7020e2c61e7d473
sigurdfandersen-sudo <sigurdf.andersen@gmail.com>
2026-09-19 20:14:09 +0200

    Forbedring av visualisering, funker, men trenger arbeid

--- Radar arduino-prosjekt/Visualisering/visual.py
+++ Radar arduino-prosjekt/Visualisering/visual.py
@@ -8,24 +8,78 @@ import math #håndtere matte, trig og vinkel
 pygame.init()
 skjerm = pygame.display.set_mode((800, 600)) #lagre variabel for farger senere
 ser = serial.Serial('COM3', 9600, timeout=1)
+sentrum_x = 400
+sentrum_y = 500
+skalering_pil = 2
+skalering_ruter = 1
+x_kordinat = 400
+y_kordinat = 500
 
+#Farger
+mørk_grønn = (0, 255, 0)
+rød = (255, 0, 0)
+steg  = 0
+
+hindring_liste = {}
 while True:
     for event in pygame.event.get(): #Pygame hendelser handler kun om hva brukeren gjør vinduet og tastatur
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    
+
     if ser.in_waiting > 0: #sjekker om det har kommet ny data inn i bufferen
         linje = ser.readline().decode('utf-8').strip() #leser data frem til linjeskift. ser.readline henter den råe dataen, decode gjør det om til tekst, strip fjerner usynlig linjeskift
-        try: 
-            vinkel_str, distanse_str, melding = linje.split(',') #
-            vinkel = int(vinkel_str)
+        try: #Håndterer kun mottak og omregning av data
+            vinkel_str, distanse_str, melding = linje.split(',') #Splitter opp linjen ved hvert komma, og lager en liste med bitene
+            vinkel = int(vinkel_str) #Gjør om fra tekst til heltall
             distanse = int(distanse_str)
+
+            vinkel_rad = math.radians(vinkel) #Gjør om grader til radianer
+            
+            x_kordinat = sentrum_x + skalering_pil * distanse * math.cos(vinkel_rad) #skalering brukes for at 1cm skal bli 3 pixler
+            y_kordinat = sentrum_y - skalering_pil * distanse * math.sin(vinkel_rad) #Er minus siden origo er øverst til venstre, og vi skal nedover
+
+
+
             print(f"Vinkel: {vinkel}, Distanse:{distanse} cm, Status:{melding}")
+            print(f"X-kordinat: {x_kordinat}, Y-kordinat: {y_kordinat} ")
+            hindring_liste[vinkel] = (x_kordinat, y_kordinat)
+
+
+
+            
+
         except ValueError:
             pass
 
-        print(f"Vinkel: {vinkel}, Distanse:{distanse} cm, Status:{melding}") 
 
     skjerm.fill((10, 30, 10))
+
+    pygame.draw.line(skjerm, mørk_grønn, (sentrum_x, sentrum_y), (x_kordinat, y_kordinat), 3) #må være plassert her for den skal oppdateres hver eneste runde, uavhengig av data
+    pygame.draw.line(skjerm, mørk_grønn, ((sentrum_x - 400, sentrum_y)), (sentrum_x + 400, sentrum_y), 2)
+    pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 400 * skalering_ruter, 1)
+    pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 300 * skalering_ruter , 1)
+    pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 200 * skalering_ruter, 1)
+    pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 100 * skalering_ruter, 1)
+    pygame.draw.circle(skjerm, rød, (x_kordinat, y_kordinat), 5, 5) 
+
+    for punkt in hindring_liste.values():
+        pygame.draw.circle(skjerm, rød, (int(punkt[0]),int(punkt[1])), 2, 2)
+
+    for vinkel in hindring_liste.keys():
+        ny_kordinat = hindring_liste[vinkel]
+        forrige_vinkel = vinkel - 5
+
+        if forrige_vinkel in hindring_liste:
+            forrige_kordinat = hindring_liste[forrige_vinkel]
+            start_punkt = (int (ny_kordinat[0]), int (ny_kordinat[1]))
+            slutt_punkt = (int (forrige_kordinat[0]), int (forrige_kordinat[1]))
+            pygame.draw.line(skjerm, rød, start_punkt, slutt_punkt, 1)
+
+
+    
+        
+
     pygame.display.flip()
