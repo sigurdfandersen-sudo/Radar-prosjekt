259ed54443675b0e3cc964f84942ed406f9548b4
sigurdfandersen-sudo <sigurdf.andersen@gmail.com>
2026-09-20 18:43:43 +0200

    bedre visualisering

--- Radar arduino-prosjekt/Visualisering/visual.py
+++ Radar arduino-prosjekt/Visualisering/visual.py
@@ -8,6 +8,8 @@ import math #håndtere matte, trig og vinkel
 pygame.init()
 skjerm = pygame.display.set_mode((800, 600)) #lagre variabel for farger senere
 ser = serial.Serial('COM3', 9600, timeout=1)
+font = pygame.font.SysFont('Arial', 20)
+font2 = pygame.font.SysFont('Arial', 50)
 sentrum_x = 400
 sentrum_y = 500
 skalering_pil = 1
@@ -20,6 +22,7 @@ y_sweep = 0
 #Farger
 mørk_grønn = (0, 255, 0)
 rød = (255, 0, 0)
+hvit = (255, 255, 255)
 
 maks_distanse = 400
 hindring_liste = {}
@@ -65,11 +68,24 @@ while True:
     pygame.draw.line(skjerm, mørk_grønn, (sentrum_x, sentrum_y), (x_kordinat, y_kordinat), 3) #må være plassert her for den skal oppdateres hver eneste runde, uavhengig av data
     pygame.draw.line(skjerm, (150,0,150), (sentrum_x, sentrum_y), (x_sweep, y_sweep), 2)
     pygame.draw.line(skjerm, mørk_grønn, ((sentrum_x - 400, sentrum_y)), (sentrum_x + 400, sentrum_y), 2)
+    pygame.draw.line(skjerm, mørk_grønn, (sentrum_x, sentrum_y), (sentrum_x + math.cos(3.14/2) * maks_distanse * skalering_pil, sentrum_y - math.sin(3.14/2) * maks_distanse * skalering_pil), 1)
+    
     pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 400 * skalering_ruter, 1)
     pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 300 * skalering_ruter , 1)
     pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 200 * skalering_ruter, 1)
     pygame.draw.circle(skjerm, mørk_grønn, (400, 500), 100 * skalering_ruter, 1)
     pygame.draw.circle(skjerm, rød, (x_kordinat, y_kordinat), 5, 5) 
+    tekst_100cm = font.render("100cm", True, hvit)
+    tekst_200cm = font.render("200cm", True, hvit)
+    tekst_300cm = font.render("300cm", True, hvit)
+    tekst_400cm = font.render("400cm", True, hvit)
+    overskrift = font2.render("Radarmåling", True, hvit)
+
+    skjerm.blit(tekst_100cm, (sentrum_x - 100, sentrum_y))
+    skjerm.blit(tekst_200cm, (sentrum_x - 200, sentrum_y))
+    skjerm.blit(tekst_300cm, (sentrum_x - 300, sentrum_y))
+    skjerm.blit(tekst_400cm, (sentrum_x - 400, sentrum_y))
+    skjerm.blit(overskrift, (sentrum_x - 100, 0))
 
     for punkt in hindring_liste.values():
         pygame.draw.circle(skjerm, rød, (int(punkt[0]),int(punkt[1])), 2, 2)
