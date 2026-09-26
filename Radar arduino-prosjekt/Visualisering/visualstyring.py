31e7f91f214b4dfc6c2213c0cfc77db24f1378ef
sigurdfandersen-sudo <sigurdf.andersen@gmail.com>
2026-09-26 15:46:01 +0200

    Forbedring, skal funke men er utestet

--- Radar arduino-prosjekt/Visualisering/visualstyring.py
+++ Radar arduino-prosjekt/Visualisering/visualstyring.py
@@ -7,6 +7,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 ser = serial.Serial('COM3', 9600, timeout = 1) #tell which port to send info with Arduino
 clock = pygame.time.Clock() #FPS
+angle_send = 0
+d_angle_send = 0
 
 while True:
     clock.tick(30) #FPS
@@ -15,24 +17,43 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
+
             if event.key == pygame.K_LEFT:
-                ser.write(b'L')
-                print("Kommando: Venstre")
+                if angle_send >= 3:
+                    d_angle_send = -3
+                    angle_send += d_angle_send
+                    angle_str = str(angle_send)
+                    angle_text = angle_str + "\n"
+                    angle_bytes = angle_text.encode("ascii")
+                    ser.write(angle_bytes)
+                    print("Kommando: Venstre")
+
+
             elif  event.key == pygame.K_RIGHT:
-                ser.write(b'R')
-                print("Kommando: Høyre")
+                if angle_send <= 177:
+                    d_angle_send = 3
+                    angle_send += d_angle_send
+                    angle_str = str(angle_send)
+                    angle_text = angle_str + "\n"
+                    angle_bytes = angle_text.encode("ascii")
+                    ser.write(angle_bytes)
+                    print("Kommando: Høyre")
+                    
+
+
+            
 
     if ser.in_waiting > 0: #sjekker om det har kommet ny data inn i bufferen
         line = ser.readline().decode('utf_8').strip() #Reading data from Arduino, ser.readline gather raw bytes, decode makes it as text, strip deletes hidden text
-        angle, distance, message = line.split(',')
+        angle, distance = line.split(',')
         angle = int(angle)
         angle_rad = math.radians(angle)
         distance = int(distance)
-
-        print(f"Angle:"{angle}, "Distance:"{distance}, "Message:"{message})
+        print(f"Angle: {angle}, Distance: {distance}")  #{} means put the value of the variable here
 
 
     pygame.display.flip() #Update the frame
+
 
 
 
--- Radar arduino-prosjekt/src/roterenderadarstyring.cpp
+++ Radar arduino-prosjekt/src/roterenderadarstyring.cpp
@@ -5,17 +5,46 @@
 const int echoPin = 3;
 const int trigPin = 10;
 const int servoPin = 11;
-
+const float soundSpeed = 0.0345;
 Servo myServo;
 
+unsigned long radarTime;
+unsigned long radarDistance;
 void setup() {
 
     pinMode(echoPin, INPUT);
     pinMode(trigPin, OUTPUT);
     pinMode(servoPin, OUTPUT);
     myServo.attach(servoPin);
-
+    Serial.begin(9600);
 }
+
+void radarRead(){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    radarTime = pulseIn(echoPin, HIGH, 25000);
+    radarDistance = soundSpeed * radarTime / 2;
+}
+
+void loop(){
+    if (Serial.available() > 0) {   //checks if there are any new messages from python
+        String receivedText = Serial.readStringUntil('\n');
+        int receivedAngle = receivedText.toInt();
+        myServo.write(receivedAngle);
+        radarRead();
+        Serial.print(receivedAngle);
+        Serial.print(",");
+        Serial.println(radarDistance);
+
+
+
+    }
+}
+
 
 
 
